a9f44f8d6e45c16d0d9c410f47e4dea63f46758b
Adam Juhasz <hello@ajuhasz.io>
2017-03-31 17:52:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const Alana = require('@alana/core').default;
 const request = require('request-promise');
+const flatten = require('lodash.flatten');
+const Promise = require('bluebird');
+const fs = require('fs');
+const path = require('path');
+const vm = require('vm');
 // Converts FB payloads to Alana common message format
 const inputConverter = require('@alana/platform-facebook').mapFBToInternal;
 // Converts alana format to FB messenger
@@ -19,28 +24,52 @@ exports.runbot = function runbot(req, res) {
   }
   
   const bot = new Alana();
-  bot.debugOn();
+  const global = {
+      newScript: bot.newScript.bind(bot),
+      addGreeting: bot.addGreeting.bind(bot),
+      request: request,
+      addErrorHandler: bot.addErrorHandler.bind(bot),
+      getScript: bot.getScript.bind(bot),
+      _bot: bot,
+  };
+  var listing = fs.readdirSync('./scripts');
+  listing.map(file => path.join(__dirname, 'scripts', file))
+    .forEach(file => {
+      vm.runInNewContext(fs.readFileSync(file, 'utf8'), global, {
+            filename: file,
+            displayErrors: true,
+        });
+    });
+  bot.turnOnDebug();
   const platform = new PlatformSnub();
-  const message = inputConverter(req.body);
-  if (message !== null) {
-    const user = {
-      _platform: this,
-      id: event.sender.id,
-      platform: 'Facebook',
-    };
-    if (bot.debugOn) {
-      console.log(`Processing ${message.type} message for ${user.id}`);
-    }
-    bot.processMessage(user, message)
+  console.log(platform);
+  const messagingEvents = flatten(req.body.entry.map(entry => entry.messaging));
+  if (bot.debugOn) {
+    console.log(`Recieved ${messagingEvents.length} messages`);
+  }
+  Promise.each(messagingEvents, (event) => {
+    const message = inputConverter(event, 'GET_STARTED_PAYLOAD');
+      if (message !== null) {
+        const user = {
+          _platform: platform,
+          id: event.sender.id,
+          platform: 'Facebook',
+        };
+        if (bot.debugOn) {
+          console.log(`Processing ${message.type} message for ${user.id}`);
+        }
+        return bot.processMessage(user, message);
+      }
+      return Promise.resolve();
+    })
       .then(() => {
-        // function terminates when a response is sent
+        // function terminates when each message is processed
         res.sendStatus(200);
       })
       .catch((err) => {
         console.error(err);
         res.sendStatus(500);
       });
-  }
 }
 
 // platform snub to respond back 
@@ -54,6 +83,7 @@ class PlatformSnub {
   send(user, message) {
     const fbmessage = outputConverter(message);
     fbmessage.recipient.id = user.id;
+    console.log(`sending ${message.type} message to ${user.id}`);
     return request({
       uri: 'https://graph.facebook.com/v2.6/me/messages',
       method: 'POST',
